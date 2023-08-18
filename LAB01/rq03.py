d07e31a57394f04025e7652c7f7c4ef25e81187d
Luiz Gustavo Santos Soares <luiz.soares.1310151@sga.pucminas.br>
2023-08-18 16:59:39 -0300

    RQ03 feita

--- LAB01/rq03.py
+++ LAB01/rq03.py
@@ -1,1 +1,43 @@
-#
+import requests
+from config import access_token
+from datetime import datetime
+
+def get_repositories():
+    url = 'https://api.github.com/graphql'
+    query = '''
+    {
+        search(query: "stars:>1", type: REPOSITORY, first: 100) {
+            edges {
+                node {
+                    ... on Repository {
+                  	    name
+                        releases (orderBy: {field: CREATED_AT, direction: DESC}){
+                            totalCount
+                        }
+                    }
+                }
+            }
+        }
+    } '''
+    header_config = {
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    
+    response = requests.post(url, json={'query': query}, headers=header_config)
+
+    if response.status_code == 200:
+
+        data = response.json()
+        repositories = data['data']['search']['edges']
+
+        for repository in repositories:
+            repo_data = repository['node']
+            print(f"Repositório: {repo_data['name']}")
+            print(f"Total Releases: {repo_data['releases']['totalCount']} releases")
+            print("**********")
+    else:
+        print("deu ruim")
+
+if __name__ == "__main__":
+    get_repositories()
