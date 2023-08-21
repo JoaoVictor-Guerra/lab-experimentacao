3389a9a712804c377332a3680e5e6351978b42db
Gabriel Victor <gabriel.victorc13@gmail.com>
2023-08-21 10:52:26 -0300

    doc(readme): Instruções de instalação

--- LAB01/rq05.py
+++ LAB01/rq05.py
@@ -1,1 +1,52 @@
-#
+import requests
+from config import access_token
+from datetime import datetime
+
+
+def get_last_update_age(updated_at):
+    date_now = datetime.now()
+    update_date = datetime.strptime(updated_at, "%Y-%m-%dT%H:%M:%SZ")
+    age = date_now - update_date
+    if(age.days<0):
+      return 0
+    return age.days
+
+
+def get_repositories():
+    url = 'https://api.github.com/graphql'
+    query = '''
+    {
+      search(query: "stars:>20", type: REPOSITORY, first: 5) {
+        edges {
+          node {
+            ... on Repository {
+              name
+              updatedAt
+            }
+          }
+        }
+      }
+    } '''
+    header_config = {
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    response = requests.post(url, json={'query': query}, headers=header_config)
+
+    if response.status_code == 200:
+
+        data = response.json()
+        repositories = data['data']['search']['edges']
+
+        for repository in repositories:
+            repo_data = repository['node']
+            update_age = get_last_update_age(repo_data['updatedAt'])
+            print(f"Repositório: {repo_data['name']}")
+            print(f"Idade update: {update_age} dias")
+            print("**********")
+    else:
+        print("deu ruim")
+
+
+if __name__ == "__main__":
+    get_repositories()
--- README.md
+++ README.md
@@ -5,3 +5,14 @@
 - Gabriel Victor Couto Martins de Paula
 - João Victor Guerra Prata Lima
 - Luiz Gustavo Santos Soares
+
+## Instalação
+### 1. Acesse um dos diretórios lab
+```sh
+cd LABO1
+```
+
+### 2. Instale as Dependências
+```sh
+pip install -r requirements.txt
+```
