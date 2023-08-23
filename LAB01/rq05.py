ce0b2349254e7fcddac0e92767734e3a47d0f394
Gabriel Victor <gabriel.victorc13@gmail.com>
2023-08-23 10:17:01 -0300

    feat(rq05): RQ05 feito

--- LAB01/rq05.py
+++ LAB01/rq05.py
@@ -1,27 +1,60 @@
 import requests
 from config import access_token
 from datetime import datetime
+from enum import Enum
+
+class TopProgrammingLanguages(Enum):
+    JAVASCRIPT = 'JavaScript'
+    PYTHON = 'Python'
+    JAVA = 'Java'
+    TYPESCRIPT = 'Typescript'
+    C_SHARP = 'C#'
+    CPP = 'C++'
+    PHP = 'PHP'
+    SHELL = 'Shell'
+    C = 'C'
+    RUBY = 'Ruby'
 
 
 def get_last_update_age(updated_at):
     date_now = datetime.now()
     update_date = datetime.strptime(updated_at, "%Y-%m-%dT%H:%M:%SZ")
     age = date_now - update_date
-    if(age.days<0):
-      return 0
+    if (age.days < 0):
+        return 0
     return age.days
 
+programming_languages_with_values = [
+    {"language": TopProgrammingLanguages.JAVASCRIPT, "value": 1},
+    {"language": TopProgrammingLanguages.PYTHON, "value": 2},
+    {"language": TopProgrammingLanguages.JAVA, "value": 3},
+    {"language": TopProgrammingLanguages.TYPESCRIPT, "value": 4},
+    {"language": TopProgrammingLanguages.C_SHARP, "value": 5},
+    {"language": TopProgrammingLanguages.CPP, "value": 6},
+    {"language": TopProgrammingLanguages.PHP, "value": 7},
+    {"language": TopProgrammingLanguages.SHELL, "value": 8},
+    {"language": TopProgrammingLanguages.C, "value": 9},
+    {"language": TopProgrammingLanguages.RUBY, "value": 10}
+]
 
 def get_repositories():
     url = 'https://api.github.com/graphql'
     query = '''
     {
-      search(query: "stars:>20", type: REPOSITORY, first: 5) {
+      search(query: "stars:>20", type: REPOSITORY, first: 25) {
         edges {
           node {
             ... on Repository {
               name
               updatedAt
+              languages(first: 3, orderBy: {field: SIZE, direction: DESC}) {
+                edges {
+                  node {
+                    id
+                    name
+                  }
+                }
+              }
             }
           }
         }
@@ -41,8 +74,14 @@ def get_repositories():
         for repository in repositories:
             repo_data = repository['node']
             update_age = get_last_update_age(repo_data['updatedAt'])
+            repo_langs = repo_data['languages']['edges']
+            languages = []
+            for language in repo_langs:
+                lang_data = language['node']
+                languages.append(lang_data['name'])
             print(f"Repositório: {repo_data['name']}")
             print(f"Idade update: {update_age} dias")
+            print(f"Linguagens: {languages}")
             print("**********")
     else:
         print("deu ruim")
