78f9b2bd3056d6988da05d2e7adfe7d3e755dab7
Luiz Gustavo Santos Soares <luiz.soares.1310151@sga.pucminas.br>
2023-08-28 16:35:20 -0300

    RQ04 com paginação
    RQ04 feita com paginação, além de sutis ajustes para facilitar a leitura do código.

--- LAB01/rq04.py
+++ LAB01/rq04.py
@@ -7,8 +7,6 @@ def get_last_update_age(updated_at):
     date_now = datetime.now()
     update_date = datetime.strptime(updated_at, "%Y-%m-%dT%H:%M:%SZ")
     age = date_now - update_date
-    if(age.days<0):
-      return 0
     return age.days
 
 
@@ -16,10 +14,15 @@ def get_repositories():
     url = 'https://api.github.com/graphql'
     query = '''
     {
-      search(query: "stars:>20", type: REPOSITORY, first: 100) {
+      search(query: "stars:>100", type: REPOSITORY, first: 100, after: null) {
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
         edges {
           node {
             ... on Repository {
+              url
               name
               updatedAt
             }
@@ -27,25 +30,39 @@ def get_repositories():
         }
       }
     } '''
+    pagination_query = query
+
     header_config = {
         'Authorization': f'Bearer {access_token}'
     }
 
-    response = requests.post(url, json={'query': query}, headers=header_config)
+    for i in range(0, 10):
+        response = requests.post(
+            url, json={'query': pagination_query}, headers=header_config)
 
-    if response.status_code == 200:
+        if response.status_code == 200:
 
-        data = response.json()
-        repositories = data['data']['search']['edges']
+            data = response.json()
+            repositories = data['data']['search']['edges']
 
-        for repository in repositories:
-            repo_data = repository['node']
-            update_age = get_last_update_age(repo_data['updatedAt'])
-            print(f"Repositório: {repo_data['name']}")
-            print(f"Idade update: {update_age} dias")
-            print("**********")
-    else:
-        print("deu ruim")
+            for repository in repositories:
+                repo_data = repository['node']
+                update_age = get_last_update_age(repo_data['updatedAt'])
+                print(f"Repositório: {repo_data['name']}")
+                print(f"Link: {repo_data['url']}")
+                print(f"Idade update: {update_age} dias")
+                print("******************")
+
+            has_next_page = data['data']['search']['pageInfo']['hasNextPage']
+            if has_next_page == False:
+                break
+            
+            end_cursor = (
+                f"\"{data['data']['search']['pageInfo']['endCursor']}\"")
+            pagination_query = query.replace('null', end_cursor)
+
+        else:
+            print("--------------DEU RUIM--------------")
 
 
 if __name__ == "__main__":
