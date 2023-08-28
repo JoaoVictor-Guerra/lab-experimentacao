7184e40f1061c0d225f5828604a2004841dc5b89
Luiz Gustavo Santos Soares <luiz.soares.1310151@sga.pucminas.br>
2023-08-28 16:04:10 -0300

    RQ03 com paginação finalizada
    Agora a RQ03 está buscando 1000 repositórios através de paginação

--- LAB01/rq03.py
+++ LAB01/rq03.py
@@ -1,43 +1,60 @@
 import requests
 from config import access_token
-from datetime import datetime
 
 def get_repositories():
     url = 'https://api.github.com/graphql'
     query = '''
     {
-        search(query: "stars:>1", type: REPOSITORY, first: 100) {
-            edges {
-                node {
-                    ... on Repository {
-                  	    name
-                        releases (orderBy: {field: CREATED_AT, direction: DESC}){
-                            totalCount
-                        }
-                    }
-                }
-            }
+      search(query: "stars:>100", type: REPOSITORY, first: 100, after: null) {
+        pageInfo {
+          hasNextPage
+          endCursor
         }
-    } '''
+        edges {
+          node {
+            ... on Repository {
+              name
+              url
+              releases(orderBy: {field: CREATED_AT, direction: DESC}) {
+                  totalCount
+              }
+            }
+          }
+        }
+      }
+    }'''
+    pagination_query = query
+    
     header_config = {
         'Authorization': f'Bearer {access_token}'
     }
 
-    
-    response = requests.post(url, json={'query': query}, headers=header_config)
+    for i in range(0, 10):
+        response = requests.post(
+            url, json={'query': pagination_query}, headers=header_config)
+        
+        if response.status_code == 200:
+            data = response.json()
+            repositories = data['data']['search']['edges']
+            
+            for repository in repositories:
+                repo_data = repository['node']
+                print(f"Repositório: {repo_data['name']}")
+                print(f"Link: {repo_data['url']}")
+                print(
+                    f"Total Releases: {repo_data['releases']['totalCount']} releases")
+                print("**********")
+            has_next_page = data['data']['search']['pageInfo']['hasNextPage']
+            
+            if has_next_page == False:
+                break
+            
+            end_cursor = (
+                f"\"{data['data']['search']['pageInfo']['endCursor']}\"")
+            pagination_query = query.replace('null', end_cursor)
 
-    if response.status_code == 200:
-
-        data = response.json()
-        repositories = data['data']['search']['edges']
-
-        for repository in repositories:
-            repo_data = repository['node']
-            print(f"Repositório: {repo_data['name']}")
-            print(f"Total Releases: {repo_data['releases']['totalCount']} releases")
-            print("**********")
-    else:
-        print("deu ruim")
+        else:
+            print("--------------DEU RUIM--------------")
 
 if __name__ == "__main__":
     get_repositories()
