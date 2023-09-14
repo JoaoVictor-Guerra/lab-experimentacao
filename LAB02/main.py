b5bc62c4e1eb059bb479a105884d6ec22eb30888
Luiz Gustavo Santos Soares <luiz.soares.1310151@sga.pucminas.br>
2023-09-13 22:24:56 -0300

    (fix)Correção de nomes e calculo de métrica
    adequação dos nomes das variáveis para facilitar legibilidade, correção da sumarização das métricas e exclusão de imports inuteis

--- LAB02/main.py
+++ LAB02/main.py
@@ -1,8 +1,6 @@
-import subprocess
 import pickle
 import os
 import pandas as pd
-import shutil
 
 
 path = os.path.dirname(__file__)
@@ -10,13 +8,15 @@ path_repo_clone = os.path.join(path, 'repoClone.py')
 path_stats = os.path.join(path, 'stats')
 path_repos = os.path.join(path, 'repos')
 
-csv1_path = f"{path_stats}/github_data.csv"
-csv2_path = f"{path_stats}/metricas_repo.csv"
+csv_git_path = f"{path_stats}/github_data.csv"
+csv_ck_path = f"{path_stats}/metricas_repo.csv"
 output_csv_path = f"{path_stats}/metricas_repo_combined.csv"  
 
 def clone_repo(repo):
     try:
         os.chdir(path)
+
+        os.system("mkdir repos")
 
         command_clone = f"python {path_repo_clone} {repo}"
 
@@ -38,7 +38,7 @@ def dump_repo():
         for f in files:
             os.unlink(os.path.join(root, f))
         for d in dirs:
-            shutil.rmtree(os.path.join(root, d))
+            os.system(f"rd /s /q {path_repos}")#shutil.rmtree(os.path.join(root, d))# "rd /s /q caminho"
 
 
 
@@ -46,10 +46,10 @@ def generate_csv():
     csv_path = "stats/class.csv"
     df = pd.read_csv(csv_path, comment='#')
     
-    total_lcom = df["lcom"].sum()
+    total_lcom = df["lcom"].median()
     total_loc = df["loc"].sum()
-    total_cbo = df["cbo"].sum()
-    total_dit = df["dit"].sum()
+    total_cbo = df["cbo"].median()
+    total_dit = df["dit"].max()
     
     totals_df = pd.DataFrame({
         "LCOM": [total_lcom],
@@ -61,12 +61,14 @@ def generate_csv():
     new_csv = "stats/metricas_repo.csv"
     totals_df.to_csv(new_csv, index=False)
 
-def make_full_cv(csv1_path, csv2_path, output_csv_path):
-    df1 = pd.read_csv(csv1_path)
-    df2 = pd.read_csv(csv2_path)
+def make_full_cv(csv_git_path, csv_ck_path, output_csv_path):
+    df1 = pd.read_csv(csv_git_path)
+    df2 = pd.read_csv(csv_ck_path)
     
     result_df = pd.concat([df1, df2], ignore_index=True)
     
+    print(result_df)
+
     result_df.to_csv(output_csv_path, index=False)
 
 if __name__ == "__main__":
@@ -75,5 +77,5 @@ if __name__ == "__main__":
 
     clone_repo(repos[0])
     generate_csv()
-    make_full_cv(csv1_path, csv2_path, output_csv_path)  
+    make_full_cv(csv_git_path, csv_ck_path, output_csv_path)  
     dump_repo()
