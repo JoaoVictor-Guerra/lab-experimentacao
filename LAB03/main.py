b6192fdeadbf23f7f5e727094dd1257c50dc8b21
JoaoVictor-Guerra <84593164+JoaoVictor-Guerra@users.noreply.github.com>
2023-10-23 10:43:04 -0300

    LAB03S02 code done

--- LAB03/main.py
+++ LAB03/main.py
@@ -2,13 +2,15 @@ import csv
 import requests
 from config import access_token
 from datetime import datetime, timedelta
-
+import time
+from requests.exceptions import ChunkedEncodingError, HTTPError
+import urllib3.exceptions
 
 def get_pr_data(owner, name):
     query = """
     query ($owner: String!, $name: String!, $cursor: String) {
         repository(owner: $owner, name: $name) {
-            pullRequests(first: 100, after: $cursor, orderBy: {field: CREATED_AT, direction: ASC}) {
+            pullRequests(first: 100, after: $cursor) {
                 edges {
                     node {
                         author {
@@ -48,7 +50,7 @@ def get_pr_data(owner, name):
 
     headers = {'Authorization': f'Bearer {access_token}'}
     url = 'https://api.github.com/graphql'
-    
+
     cursor = None
     pr_data = []
 
@@ -62,15 +64,18 @@ def get_pr_data(owner, name):
             }
         }
 
-        response = requests.post(url, json=data, headers=headers)
-
-        if response.status_code != 200:
-            print(f"Erro {response.status_code}")
-            break
+        try:
+            response = requests.post(url, json=data, headers=headers)
+            response.raise_for_status()  
+        except (ChunkedEncodingError, HTTPError, urllib3.exceptions.HTTPError) as e:
+            print(f"Erro na solicitação: {e}")
+            time.sleep(5)  
+            continue
 
         data = response.json()
-        
+
         pull_requests = data.get('data', {}).get('repository', {}).get('pullRequests', {}).get('edges', [])
+        hasNextPage = data.get('data', {}).get('repository', {}).get('pullRequests', {}).get('pageInfo', {}).get('hasNextPage', False)
 
         if not pull_requests:
             break
@@ -82,62 +87,56 @@ def get_pr_data(owner, name):
             merged_at = pr.get('mergedAt')
 
             if created_at and (closed_at or merged_at):
-                created_time = datetime.fromisoformat(created_at[:-1])
-                closed_time = datetime.fromisoformat(closed_at[:-1]) if closed_at else None
-                merged_time = datetime.fromisoformat(merged_at[:-1]) if merged_at else None
+                pr_data.append({
+                    'nameWithOwner': f"{owner}/{name}",
+                    'title': pr.get('title', ''),
+                    'state': pr.get('state', ''),
+                    'createdAt': created_at,
+                    'closed': pr.get('closed', False),
+                    'merged': pr.get('merged', False),
+                    'closedAt': closed_at,
+                    'mergedAt': merged_at,
+                    'reviews_totalCount': pr.get('reviews', {}).get('totalCount', 0),
+                    'participants_totalCount': pr.get('participants', {}).get('totalCount', 0),
+                    'comments_totalCount': pr.get('comments', {}).get('totalCount', 0),
+                    'changedFiles': pr.get('changedFiles', 0),
+                    'deletions': pr.get('deletions', 0),
+                    'body': pr.get('body', '')
+                })
 
-                time_threshold = timedelta(hours=1)
-
-                if (closed_time and closed_time - created_time > time_threshold) or (merged_time and merged_time - created_time > time_threshold):
-                    pr_data.append({
-                        'nameWithOwner': f"{owner}/{name}",
-                        'title': pr.get('title', ''),
-                        'state': pr.get('state', ''),
-                        'createdAt': created_at,
-                        'closed': pr.get('closed', False),
-                        'merged': pr.get('merged', False),
-                        'closedAt': closed_at,
-                        'mergedAt': merged_at,
-                        'reviews_totalCount': pr.get('reviews', {}).get('totalCount', 0),
-                        'participants_totalCount': pr.get('participants', {}).get('totalCount', 0),
-                        'comments_totalCount': pr.get('comments', {}).get('totalCount', 0),
-                        'changedFiles': pr.get('deletions', 0),
-                        'deletions': pr.get('deletions', 0),
-                        'body': pr.get('body', '')
-                    })
-
-        hasNextPage = data.get('data', {}).get('repository', {}).get('pullRequests', {}).get('pageInfo', {}).get('hasNextPage', False)
         if not hasNextPage:
             break
 
         cursor = data.get('data', {}).get('repository', {}).get('pullRequests', {}).get('pageInfo', {}).get('endCursor', None)
+        time.sleep(2)  
 
     return pr_data
 
-csv_filename = 'analyzed_repositories.csv'
-repositories = []
+def main():
+    csv_filename = 'analyzed_repositories.csv'
+    repositories = []
 
-with open(csv_filename, 'r') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        nameWithOwner = row['nameWithOwner']
-        owner, name = nameWithOwner.split('/')
-        repositories.append({'nameWithOwner': nameWithOwner, 'owner': owner, 'name': name})
+    with open(csv_filename, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            nameWithOwner = row['nameWithOwner']
+            owner, name = nameWithOwner.split('/')
+            repositories.append({'nameWithOwner': nameWithOwner, 'owner': owner, 'name': name})
 
+    for repo in repositories[:70]:  # Coleta dos 70 primeiros repositórios
+        owner = repo['owner']
+        name = repo['name']
+        pull_requests = get_pr_data(owner, name)
 
-if repositories:
-    repo = repositories[0]  # Primeiro repositório
-    owner = repo['owner']
-    name = repo['name']
-    pull_requests = get_pr_data(owner, name)
+        with open('pr_data.csv', 'a', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['nameWithOwner', 'title', 'state', 'createdAt', 'closed', 'merged',
+                        'closedAt', 'mergedAt', 'reviews_totalCount', 'participants_totalCount',
+                        'comments_totalCount', 'changedFiles', 'deletions', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
+            if csvfile.tell() == 0:
+                writer.writeheader()
+            writer.writerows(pull_requests)
 
-    with open('pr_data.csv', 'w', newline='') as csvfile:
-        fieldnames = ['nameWithOwner', 'title', 'state', 'createdAt', 'closed', 'merged',
-                      'closedAt', 'mergedAt', 'reviews_totalCount', 'participants_totalCount',
-                      'comments_totalCount', 'changedFiles', 'deletions', 'body']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(pull_requests)
-else:
-    print("lascou")
+if __name__ == "__main__":
+    main()
